59f1c5869e8f537fb870c0108594d5e20814181f
ravi0497 <muhammadravi04@gmail.com>
2020-03-04 11:16:16 +0700

    laporan

--- imageclassification.py
+++ imageclassification.py
@@ -6,18 +6,14 @@ Created on Wed Feb 12 05:52:20 2020
 """
 
 import cv2
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from urllib.request import urlopen
+import warnings
+#warnings.filterwarnings("ignore")
 
-#membaca image dari url
-def readImg(url):
-    resp=urlopen(url)
-    image = np.asarray(bytearray(resp.read()),dtype="uint8")
-    image= cv2.imdecode(image,cv2.IMREAD_COLOR)
-    
-    return image
 
     
 #eksplorasi historgram
@@ -55,6 +51,12 @@ def countColor(image):
     
             plt.show()    
 
+cat1 = cv2.imread('cat-example.jpg',3)
+conv1=cv2.cvtColor(cat1, cv2.COLOR_BGR2LAB) 
+cv2.imshow('image',conv1)
+cv2.waitKey(0)
+
+#countColor(conv1)
 
 #hitung dan normalisasi bins
 def calculateBins(image,bins,mask):
@@ -64,9 +66,9 @@ def calculateBins(image,bins,mask):
     histR=cv2.calcHist([image],[2],mask,[bins],[0,256])
     
     #normalisasi
-    normB=histB/len(histB)
-    normG=histG/len(histG)
-    normR=histR/len(histR)
+    normB=histB/max(histB)
+    normG=histG/max(histG)
+    normR=histR/max(histR)
     
     #concat jadi satu vektor
     bins_vector=[]
@@ -77,6 +79,7 @@ def calculateBins(image,bins,mask):
         
     return bins_vector
         
+bin_df=calculateBins(conv1,256,None)
 
 from collections import Counter
 def most_frequent(List):
@@ -106,7 +109,7 @@ def calculateBinsByWindow(image):
         
             mask = np.zeros(image.shape[:2],np.uint8)
             mask[begin_row:end_row,begin_col:end_col] = 255
-            data =calculateBins(image,255,mask)
+            data =calculateBins(image,256,mask)
             kmeans.fit(data)
             
             centroids=kmeans.cluster_centers_
@@ -136,38 +139,20 @@ def feature_selection(centroid,labels):
 
 #menghitung kesamaan fitur dengan euclidean distance
 def feature_similarity(img1):
-    lab1=cv2.cvtColor(img1, cv2.COLOR_BGR2LAB)
-#    lab2=cv2.cvtColor(img2, cv2.COLOR_BGR2LAB)
-    
-    r1=calculateBinsByWindow(lab1)
-#    r2=calculateBinsByWindow(lab2)
-    
+    lab1=cv2.cvtColor(img1, cv2.COLOR_BGR2LAB)    
+    r1=calculateBinsByWindow(lab1)    
     vector1=feature_selection(r1[0],r1[1])
-#    vector2=feature_selection(r2[0],r2[1])
-    
-    
-#    distances=[]
-#    res=0
-#    for i in range(len(vector1)):
-#        d = distance.euclidean(vector1[i], vector2[i])
-#        res+=d
-#        distances.append(d)        
-#    
-#    
-#    print(vector1)
-#    threshold.append(res)
     return np.array(vector1).reshape(-1)
-#    return vector1
+
 
 fitur=[]
 labels=[]
-debug=[]
 
 
-def insert_feature(path,file,label):
+def insert_feature(path,file,label,jumlah):
     base_beach=path
     temp=file
-    for i in range(22):
+    for i in range(jumlah):
         idx=i+1
         idx_conv=str(idx)
         name=temp+idx_conv+'.jpg'
@@ -177,47 +162,62 @@ def insert_feature(path,file,label):
         labels.append(label)
 
 
-    
-cat1 = cv2.imread('pantai-test.jpg',3)
-cat2 = cv2.imread('piaget-2.jpg',3)
-#test=feature_similarity(cat1)
-insert_feature("beach/","pantai","pantai")
-print(np.array(fitur).shape)
+            
 
-#debug=[]
-#debug.append([1,2,3])
-#debug.append([1,2,3])
-#print(np.array(debug).shape)
-insert_feature("hutan/","hutan","hutan")
+cat2 = cv2.imread('piaget-2.jpg',3)
+
+#masukan gambar-gambar
+insert_feature("beach/","pantai","pantai",47)
+print(np.array(fitur).shape)
+insert_feature("hutan/","hutan","hutan",47)
+insert_feature("snow/","snow","snow",44)
+insert_feature("gedung/","gedung","gedung",44)
 
 ##classification
 from sklearn import preprocessing
-#
+
 le=preprocessing.LabelEncoder()
 img_en=le.fit_transform(labels)
 arr_en=np.array(img_en).reshape(-1)
 
 #KNN
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-model = KNeighborsClassifier(n_neighbors=6)
-X_train, X_test, y_train, y_test = train_test_split(fitur, labels, test_size=0.3)
-model.fit(fitur,labels)
+y_real=['pantai','pantai','pantai','pantai','pantai',
+        'gedung','gedung','gedung','gedung','gedung',
+        'snow','snow','snow','snow','snow',
+        'hutan','hutan','hutan','hutan','hutan']
 
-#print(model.predict(X_test))
-#
-test=[]
-pr=feature_similarity(cat1)
-test.append(np.array(pr).reshape(-1))
+data={'label_benar':y_real}    
+dataframe=pd.DataFrame(data)
 
-#print(arr_pr.shape)
-##arr_pr=np.array(pr).reshape(-1,1)
-predicted=model.predict(test)
-print(predicted)
+for i in range(1,17):
+    model = KNeighborsClassifier(n_neighbors=i)
+    model.fit(fitur,labels)
+    
+    y_pred=[]
+    for j in range(1,21):
+        index=j
+        index_covt=str(index)
+        name="test/test"+index_covt+".jpg"
+        test_img=cv2.imread(name,3)
+        test=[]
+        pr=feature_similarity(test_img)
+        test.append(np.array(pr).reshape(-1))
+        
+        #print(arr_pr.shape)
+        ##arr_pr=np.array(pr).reshape(-1,1)
+        predicted=model.predict(test)
+        y_pred.append(predicted)
+        
+    #check akurasi
+    y_result=np.array(y_pred).reshape(-1)
+    print("akurasi untuk K=",i,": ",accuracy_score(y_result,y_real))
+    dataframe['K='+str(i)]=y_result
 
 
-
+print(dataframe)
 
 
 
